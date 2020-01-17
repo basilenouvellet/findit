46e8358b67a752650e07cc20f0d8ed3614ecec26
Basile NOUVELLET <basile.nouvellet@ponts.org>
2020-01-17 20:00:45 +0100

    :tada: Asking a question and geeting an answer is now working for Wikipedia pages

--- frontend/src/popup/src/App.tsx
+++ frontend/src/popup/src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { styled, ThemeProvider } from '@material-ui/core/styles';
+import React, { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-
-import { sendMessageToContentScript } from './utils/chrome';
+import Typography from '@material-ui/core/Typography';
+import { styled, ThemeProvider } from '@material-ui/core/styles';
 
 import theme from './theme.js';
+import { sendMessageToContentScript } from './utils/chrome';
 
 const AppContainer = styled(Box)({
   height: '30em',
@@ -29,20 +28,52 @@ const AppGridContainer = styled(Grid)({
   padding: '2em 4em',
 });
 
-const onSearchClick = () => {
-  sendMessageToContentScript(
-    { getParas: true },
-    response => {
+interface Answer{
+  answer: string,
+  score: number,
+  start: number,
+  end: number
+}
+
+const App: React.FC = () => {
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState<null | Answer>(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestion(event.target.value);
+  };
+
+  const onSearchClick = () => {
+    setAnswer(null);
+
+    sendMessageToContentScript({ getParas: true }, async response => {
       console.log('[app.js] response', response);
 
       const { paras } = response;
       const text = paras.join(' ');
-      console.log('[app.js] text', text);
-    },
-  );
-};
 
-const App: React.FC = () => {
+      console.log('[app.js] before fetch, question:', question);
+      console.log('[app.js] before fetch, text:', text);
+      
+      const rawRes = await fetch('http://localhost:5000/api/nlp', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          question,
+          context: text,
+        }),
+      });
+
+      const jsonRes = await rawRes.json();
+
+      console.log('[app.js] json response from Findit API', jsonRes);
+      setAnswer(jsonRes.data.answer);
+    });
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <AppContainer>
@@ -63,11 +94,23 @@ const App: React.FC = () => {
               label="Question"
               placeholder="Ask a question"
               variant="outlined"
+              onChange={handleChange}
               autoFocus
               fullWidth
             />
           </Grid>
-          
+
+          {answer &&
+            <Grid item xs={12}>
+              <div style={{ fontSize: '1.2em' }}>
+                <b>{answer.answer}</b>
+                <br/>
+                <br/>
+                (Score: {answer.score.toFixed(2)}%) [{answer.start} - {answer.end}]
+              </div>
+            </Grid>
+          }
+
           <Grid item xs={2}>
             <Button
               variant="contained"
